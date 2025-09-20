621281b96c52a814e94a9698ff67ce4fa21360b4
Ana Zlatkovic <zlatkovic.ana@gmail.com>
2025-09-20 15:39:50 +0200

    Auth form with react-hook-form, shadcn (label, input, form). Custom validations.

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
 import React from "react";
+import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      // onSubmit={signInWithCredentials}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignIn;
---
+++ components/forms/AuthForm.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { standardSchemaResolver } from "@hookform/resolvers/standard-schema";
+import Link from "next/link";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+import { z, ZodType } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import ROUTES from "@/constants/routes";
+
+interface AuthFormProps<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean }>;
+  formType: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  schema,
+  defaultValues,
+  formType,
+  onSubmit,
+}: AuthFormProps<T>) => {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: standardSchemaResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async () => {
+    // TODO: Authenticate User
+  };
+
+  const buttonText = formType === "SIGN_IN" ? "Sign In" : "Sign Up";
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="mt-10 space-y-6"
+      >
+        {Object.keys(defaultValues).map((fieldName) => (
+          <FormField
+            key={fieldName}
+            control={form.control}
+            name={fieldName as Path<T>}
+            render={({ field: fieldProps }) => (
+              <FormItem className="flex w-full flex-col gap-2.5">
+                <FormLabel className="paragraph-medium text-dark400_light700">
+                  {fieldName === "email"
+                    ? "Email Address"
+                    : fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    required
+                    type={fieldName === "password" ? "password" : "text"}
+                    {...fieldProps}
+                    className="paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 no-focus min-h-12 rounded-1.5 border"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ))}
+
+        <Button
+          disabled={form.formState.isSubmitting}
+          className="primary-gradient paragraph-medium min-h-12 w-full rounded-2 px-4 py-3 font-inter !text-light-900"
+        >
+          {form.formState.isSubmitting
+            ? buttonText === "Sign In"
+              ? "Signin In..."
+              : "Signing Up..."
+            : buttonText}
+        </Button>
+
+        {formType === "SIGN_IN" ? (
+          <p>
+            Don`&apos`t have an account?{" "}
+            <Link
+              href={ROUTES.SIGN_UP}
+              className="paragraph-semibold primary-text-gradient"
+            >
+              Sign up
+            </Link>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <Link
+              href={ROUTES.SIGN_IN}
+              className="paragraph-semibold primary-text-gradient"
+            >
+              Sign in
+            </Link>
+          </p>
+        )}
+      </form>
+    </Form>
+  );
+};
+
+export default AuthForm;
