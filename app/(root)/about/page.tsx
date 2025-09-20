b53b6fb7d232011a749d16877c2d7826d3b463b4
Ana Zlatkovic <zlatkovic.ana@gmail.com>
2025-09-20 18:42:31 +0200

    MobileNavigation component with shadcn sheet (react-dialog), NavLins custom component, Sidebar links as constants (constants/index.ts), and corresponding routes

--- app/(root)/about/page.tsx
+++
@@ -1,7 +0,0 @@
-const Page = () => {
-  return (
-    <div>About</div>
-  )
-}
-
-export default Page
---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tags = () => {
+  return <div>Tafs</div>;
+};
+
+export default Tags;
