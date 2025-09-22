ddd462ee0b35c32633eecf2b8a6784da87932a57
Ana Zlatkovic <zlatkovic.ana@gmail.com>
2025-09-22 15:56:58 +0200

    Implemented Editor field in Question Form

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { AskQuestionSchema } from "@/lib/validations";
-import React from "react";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
+
+import { AskQuestionSchema } from "@/lib/validations";
+
+import { Button } from "../ui/button";
 import {
   Form,
   FormControl,
@@ -14,9 +19,14 @@ import {
   FormMessage,
 } from "../ui/form";
 import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+
+const Editor = dynamic(() => import("@/components/editor"), {
+  ssr: false,
+});
 
 const QuestionForm = () => {
+  const editorRef = useRef<MDXEditorMethods>(null);
+
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -65,7 +75,13 @@ const QuestionForm = () => {
                 Detailed explanation of your problem{" "}
                 <span className="text-primary-500">*</span>
               </FormLabel>
-              <FormControl>Editor</FormControl>
+              <FormControl>
+                <Editor
+                  value={field.value}
+                  editorRef={editorRef}
+                  fieldChange={field.onChange}
+                />
+              </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you&apos;ve put in the
                 title.
@@ -76,7 +92,7 @@ const QuestionForm = () => {
         />
         <FormField
           control={form.control}
-          name="title"
+          name="tags"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col gap-3">
               <FormLabel className="paragraph-semibold text-dark400_light800">
@@ -86,7 +102,7 @@ const QuestionForm = () => {
                 <div>
                   <Input
                     className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
-                    placeholder="Add tags"
+                    placeholder="Add tags..."
                     {...field}
                   />
                   Tags
@@ -100,6 +116,7 @@ const QuestionForm = () => {
             </FormItem>
           )}
         />
+
         <div className="mt-16 flex justify-end">
           <Button
             type="submit"
