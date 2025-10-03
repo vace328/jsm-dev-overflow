f37ef7ebac11e2d30da58084c8055b03ab2af8b3
Ana <zlatkovic.ana@gmail.com>
2025-10-03 20:28:41 +0200

    Created Answer model

---
+++ database/answer.model.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAnswer {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+  content: string;
+  upwotes: number;
+  downwotes: number;
+}
+
+const AnswerSchema = new Schema<IAnswer>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+    content: { type: String, required: true },
+    upwotes: { type: Number, default: 0 },
+    downwotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Answer = models?.Answer || model<IAnswer>("Account", AnswerSchema);
+export default Answer;
