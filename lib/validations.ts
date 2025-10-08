09d0fc1e86f4361dab254db7479ebdd3e4106dfe
Ana <zlatkovic.ana@gmail.com>
2025-10-08 17:01:42 +0200

    Implemented Create & Read User routes

--- lib/validations.ts
+++ lib/validations.ts
@@ -69,13 +69,15 @@ export const AskQuestionSchema = z.object({
 });
 
 export const UserSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  username: z.string().min(3, "Username must be at least 3 characters"),
-  email: z.string().email("Invalid email address"),
+  name: z.string().min(1, { message: "User is required" }),
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long" }),
+  email: z.email({ message: "Please provide a valid email address" }),
   bio: z.string().optional(),
-  image: z.string().url("Invalid image URL").optional(),
+  image: z.url({ message: "Please provide a valid URL" }).optional(),
   location: z.string().optional(),
-  portfolio: z.string().url("Invalid portfolio URL").optional(),
+  portfolio: z.url({ message: "Please provide a valid URL" }).optional(),
   reputation: z.number().optional(),
 });
 
